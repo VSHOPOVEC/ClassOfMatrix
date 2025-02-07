1a5b560f70cde15bb05fb33b67586b524b547734
VSHOPOVEC <misha.medvedev05@mail.ru>
2025-02-07 23:55:49 +0300

    Add files via upload

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,87 +1,138 @@
 #include "include.hpp"
 
-template <typename T>
-class Matrix {
-private:
-	class MatrixRow {
-	public:
-		MatrixRow(T** in_matrix_arr,int columns) : columns(columns), matrix_arr(in_matrix_arr){}
-		T* operator[] (int index) {
-			if (index < 0 || columns < index) {
-				throw std::invalid_argument("Invalid index");
-			}
-			else {
-				return matrix_arr[index];
-			}
-		}
+
+
+namespace Matrix {
+	template <typename T>
+	class Matrix {
 	private:
-		T** matrix_arr; 
+		friend class Vector;
+		class MatrixRow {
+		public:
+			MatrixRow(T** in_matrix_arr, int columns) : columns(columns), matrix_arr(in_matrix_arr) {}
+			T* operator[] (int index) {
+				if (index < 0 || columns < index) {
+					throw std::invalid_argument("Invalid index");
+				}
+				else {
+					return matrix_arr[index];
+				}
+			}
+		private:
+			T** matrix_arr;
+			int columns;
+		};
+	public:
+
+		Matrix(std::vector<std::vector<T>> arr_massive);
+
+		Matrix(bool identity_matrix, int rows, int columns);
+
+		Matrix(int rows, int columns);
+
+		Matrix(const Matrix& matrix);
+
+		Matrix();
+
+		~Matrix();
+
+		Matrix operator + (const Matrix& matrix) const;
+
+		Matrix operator - (const Matrix& matrix) const;
+
+		Matrix operator - ()const;
+
+		Matrix operator * (const Matrix& matrix) const;
+
+		Matrix& operator = (const Matrix& matrix);
+
+		T* operator[](int index);
+
+		Matrix& TransposeMatrix();
+
+		void PrintMatrix();
+	private:
+
 		int columns;
+
+		int rows;
+
+		T** matrix_arr;
+
+		void CreateMatrixArr(int rows, int columns);
+
+		size_t InvalidValueArray(const std::vector<std::vector<T>>& arr);
 	};
-public:
-
-	Matrix(std::vector<std::vector<T>> arr_massive);
-
-	Matrix(bool identity_matrix, int rows, int columns);
-
-	Matrix(int rows, int columns);
-
-	Matrix(const Matrix& matrix);
-
-	Matrix(); 
-
-	~Matrix();
-
-	Matrix operator + (const Matrix& matrix) const;
-
-	Matrix operator - (const Matrix& matrix) const;
-
-	Matrix operator - ()const; 
-
-	Matrix operator * (const Matrix& matrix) const;
-
-	Matrix& operator = (const Matrix& matrix);
-
-	T* operator[](int index);
-
-	Matrix& TransposeMatrix();
-
-	void PrintMatrix();
-private:
-
-	int columns;
-
-	int rows;
-
-	T** matrix_arr;
-
-	void CreateMatrixArr(int rows, int columns);
-
-	size_t InvalidValueArray(const std::vector<std::vector<T>>& arr);
-};
 
 
 
-class Error;
-//Constructors Matrix
-template <typename T>
-Matrix<T>::Matrix(std::vector<std::vector<T>> arr_massive)
-{
-	size_t rowsSize = arr_massive.size();
-	size_t columnsSize = InvalidValueArray(arr_massive);
-	rows = static_cast<int>(rowsSize); columns = static_cast<int>(columnsSize);
-	CreateMatrixArr(rows, columns);
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			matrix_arr[i][j] = arr_massive[i][j];
+	class Error;
+	//Constructors Matrix
+	template <typename T>
+	Matrix<T>::Matrix(std::vector<std::vector<T>> arr_massive)
+	{
+		size_t rowsSize = arr_massive.size();
+		size_t columnsSize = InvalidValueArray(arr_massive);
+		rows = static_cast<int>(rowsSize); columns = static_cast<int>(columnsSize);
+		CreateMatrixArr(rows, columns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				matrix_arr[i][j] = arr_massive[i][j];
+			}
 		}
 	}
-}
 
-template <typename T>
-Matrix<T>::Matrix(bool identity_matrix, int rows, int columns) :rows(static_cast<int>(fabs(rows))), columns(static_cast<int>(fabs(rows))) {
-	CreateMatrixArr(this->rows, this->columns);
-	if (identity_matrix == true) {
+	template <typename T>
+	Matrix<T>::Matrix(bool identity_matrix, int rows, int columns) :rows(static_cast<int>(fabs(rows))), columns(static_cast<int>(fabs(rows))) {
+		CreateMatrixArr(this->rows, this->columns);
+		if (identity_matrix == true) {
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < columns; ++j) {
+					if (i == j) {
+						matrix_arr[i][j] = 1;
+					}
+					else {
+						matrix_arr[i][j] = 0;
+					}
+				}
+			}
+		}
+		else {
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < columns; ++j) {
+					std::cout << i << " " << j << std::endl;
+					std::cin >> matrix_arr[i][j];
+				}
+			}
+		}
+	}
+
+	template <typename T>
+	Matrix<T>::Matrix(int rows, int columns) :rows(static_cast<int>(fabs(rows))), columns(static_cast<int>(fabs(columns)))
+	{
+		CreateMatrixArr(this->rows, this->columns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				matrix_arr[i][j] = 0;
+			}
+		}
+	}
+
+	template <typename T>
+	Matrix<T>::Matrix(Matrix const& matrix) :rows(matrix.rows), columns(matrix.columns)
+	{
+		CreateMatrixArr(rows, columns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				this->matrix_arr[i][j] = matrix.matrix_arr[i][j];
+			}
+		}
+	}
+
+	template <typename T>
+	Matrix<T>::Matrix() :rows(3), columns(3)
+	{
+		CreateMatrixArr(this->rows, this->columns);
 		for (int i = 0; i < rows; ++i) {
 			for (int j = 0; j < columns; ++j) {
 				if (i == j) {
@@ -93,132 +144,85 @@ Matrix<T>::Matrix(bool identity_matrix, int rows, int columns) :rows(static_cast
 			}
 		}
 	}
-	else {
+
+	template <typename T>
+	Matrix<T>::~Matrix<T>() {
 		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < columns; ++j) {
-				std::cout << i << " " << j << std::endl;
-				std::cin >> matrix_arr[i][j];
-			}
+			delete[] matrix_arr[i];
 		}
+		delete[] matrix_arr;
 	}
-}
 
-template <typename T>
-Matrix<T>::Matrix(int rows, int columns) :rows(static_cast<int>(fabs(rows))), columns(static_cast<int>(fabs(columns)))
-{
-	CreateMatrixArr(this->rows,this->columns);
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			matrix_arr[i][j] = 0;
+
+	//Operators Matrix 
+	template <typename T>
+	Matrix<T> Matrix<T>::operator+(const Matrix& matrix) const
+	{
+		if (matrix.columns != this->columns && matrix.rows != this->rows) {
+			throw std::invalid_argument("Invalid size of matrix");
 		}
-	}
-}
-
-template <typename T>
-Matrix<T>::Matrix(Matrix const& matrix) :rows(matrix.rows), columns(matrix.columns)
-{
-	CreateMatrixArr(rows, columns);
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			this->matrix_arr[i][j] = matrix.matrix_arr[i][j];
-		}
-	}
-}
-
-template <typename T>
-Matrix<T>::Matrix() :rows(3), columns(3)
-{
-	CreateMatrixArr(this->rows, this->columns);
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			if (i == j) {
-				matrix_arr[i][j] = 1;
-			}
-			else {
-				matrix_arr[i][j] = 0;
-			}
-		}
-	}
-}
-
-template <typename T>
-Matrix<T>::~Matrix<T>() {
-	for (int i = 0; i < rows; ++i) {
-		delete[] matrix_arr[i];
-	}
-	delete[] matrix_arr;
-}
-
-
-//Operators Matrix 
-template <typename T>
-Matrix<T> Matrix<T>::operator+(const Matrix& matrix) const
-{
-	if (matrix.columns != this->columns && matrix.rows != this->rows) {
-		throw Error("Invalid size of Matrix");
-	}
-	else {
-		Matrix result(matrix.rows, matrix.columns);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < columns; ++j) {
-				result.matrix_arr[i][j] = this->matrix_arr[i][j] + matrix.matrix_arr[i][j];
-			}
-		}
-		return result;
-	}
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-(const Matrix& matrix) const
-{
-	if (matrix.columns != this->columns && matrix.rows != this->rows) {
-		throw Matrix::Error("Invalid size of Matrix");
-	}
-	else {
-		Matrix result(matrix.rows, matrix.columns);
-		for (int i = 0; i < rows; ++i) {
-			for (int j = 0; j < columns; ++j) {
-				result.matrix_arr[i][j] = this->matrix_arr[i][j] - matrix.matrix_arr[i][j];
-			}
-		}
-		return result;
-	}
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-() const
-{
-	Matrix result(this->rows, this->columns);
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			result.matrix_arr[i][j] = (this->matrix_arr[i][j]) * (-1);
-		}
-	}
-	return result;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator*(const Matrix& matrix) const
-{
-	if (matrix.rows != this->columns || matrix.columns != this->rows) {
-		throw Matrix::Error("Invalid size of Matrix");
-	}
-	else {
-		Matrix result(this->rows, matrix.columns);
-		for (int k = 0; k < this->rows; ++k) {
-			for (int i = 0; i < matrix.columns; ++i) {
-				for (int j = 0; j < this->columns; ++j) {
-					result.matrix_arr[k][i] += (this->matrix_arr[k][j]) * matrix.matrix_arr[j][i];
+		else {
+			Matrix result(matrix.rows, matrix.columns);
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < columns; ++j) {
+					result.matrix_arr[i][j] = this->matrix_arr[i][j] + matrix.matrix_arr[i][j];
 				}
 			}
+			return result;
+		}
+	}
+
+	template <typename T>
+	Matrix<T> Matrix<T>::operator-(const Matrix& matrix) const
+	{
+		if (matrix.columns != this->columns && matrix.rows != this->rows) {
+			throw std::invalid_argument("Invalid size of matrix");
+		}
+		else {
+			Matrix result(matrix.rows, matrix.columns);
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < columns; ++j) {
+					result.matrix_arr[i][j] = this->matrix_arr[i][j] - matrix.matrix_arr[i][j];
+				}
+			}
+			return result;
+		}
+	}
+
+	template <typename T>
+	Matrix<T> Matrix<T>::operator-() const
+	{
+		Matrix result(this->rows, this->columns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				result.matrix_arr[i][j] = (this->matrix_arr[i][j]) * (-1);
+			}
 		}
 		return result;
 	}
-}
 
-template <typename T>
-Matrix<T>& Matrix<T>::operator=(const Matrix& matrix)
-{
+	template <typename T>
+	Matrix<T> Matrix<T>::operator*(const Matrix& matrix) const
+	{
+		if (matrix.rows != this->columns || matrix.columns != this->rows) {
+			throw std::invalid_argument("Invalid size of matrix");
+		}
+		else {
+			Matrix result(this->rows, matrix.columns);
+			for (int k = 0; k < this->rows; ++k) {
+				for (int i = 0; i < matrix.columns; ++i) {
+					for (int j = 0; j < this->columns; ++j) {
+						result.matrix_arr[k][i] += (this->matrix_arr[k][j]) * matrix.matrix_arr[j][i];
+					}
+				}
+			}
+			return result;
+		}
+	}
+
+	template <typename T>
+	Matrix<T>& Matrix<T>::operator=(const Matrix& matrix)
+	{
 		for (int i = 0; i < rows; ++i) {
 			delete[] matrix_arr[i];
 		}
@@ -232,84 +236,83 @@ Matrix<T>& Matrix<T>::operator=(const Matrix& matrix)
 				matrix_arr[i][j] = matrix.matrix_arr[i][j];
 			}
 		}
-	return *this;
-}
-
-class MatrixRow; 
-template<typename T>
-T* Matrix<T>::operator[](int index) {
-	if (index < 0 || rows < index) {
-		throw std::invalid_argument("Invalid index of rows");
-	}
-	else {
-		return MatrixRow(matrix_arr, columns)[index];
-	}
-}
-
-//Methods of class Matrix
-template <typename T>
-void Matrix<T>::PrintMatrix() {
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < columns; ++j) {
-			std::cout << matrix_arr[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-template <typename T>
-Matrix<T>& Matrix<T>::TransposeMatrix()
-{
-	Matrix result(*this);
-	for (int i = 0; i < rows; ++i) {
-		delete[] matrix_arr[i];
-	}
-	delete[] matrix_arr;
-
-	int temp_rows = rows;
-
-	matrix_arr = new T * [columns];
-	for (int i = 0; i < columns; ++i) {
-		matrix_arr[i] = new T[rows];
+		return *this;
 	}
 
-
-	for (int i = 0; i < this->rows; ++i) {
-		for (int j = 0; j < this->columns; ++j) {
-			matrix_arr[j][i] = result.matrix_arr[i][j];
-		}
-	}
-	rows = columns; columns = temp_rows;
-	return *this;
-}
-
-template <typename T>
-void Matrix<T>::CreateMatrixArr(int rows, int columns)
-{
-	matrix_arr = new T * [rows];
-	for (int i = 0; i < rows; ++i) {
-		matrix_arr[i] = new T[columns];
-	}
-}
-template <typename T>
-size_t Matrix<T>::InvalidValueArray(const std::vector<std::vector<T>>& arr)
-{
-	size_t amountOfRows = arr.size();
-	size_t currentSizeArr = 0;
-	bool firstElement = true;
-	for (auto rowArr : arr) {
-		if (firstElement == true) {
-			currentSizeArr = rowArr.size();
-			firstElement = false;
+	class MatrixRow;
+	template<typename T>
+	T* Matrix<T>::operator[](int index) {
+		if (index < 0 || rows < index) {
+			throw std::invalid_argument("Invalid index of rows");
 		}
 		else {
-			if (rowArr.size() != currentSizeArr) {
-				throw std::invalid_argument("Invalid index");
-				break;
-			}
+			return MatrixRow(matrix_arr, columns)[index];
 		}
 	}
-	return currentSizeArr;
+
+	//Methods of class Matrix
+	template <typename T>
+	void Matrix<T>::PrintMatrix() {
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < columns; ++j) {
+				std::cout << matrix_arr[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	template <typename T>
+	Matrix<T>& Matrix<T>::TransposeMatrix()
+	{
+		Matrix result(*this);
+		for (int i = 0; i < rows; ++i) {
+			delete[] matrix_arr[i];
+		}
+		delete[] matrix_arr;
+
+		int temp_rows = rows;
+
+		matrix_arr = new T * [columns];
+		for (int i = 0; i < columns; ++i) {
+			matrix_arr[i] = new T[rows];
+		}
+
+
+		for (int i = 0; i < this->rows; ++i) {
+			for (int j = 0; j < this->columns; ++j) {
+				matrix_arr[j][i] = result.matrix_arr[i][j];
+			}
+		}
+		rows = columns; columns = temp_rows;
+		return *this;
+	}
+
+	template <typename T>
+	void Matrix<T>::CreateMatrixArr(int rows, int columns)
+	{
+		matrix_arr = new T * [rows];
+		for (int i = 0; i < rows; ++i) {
+			matrix_arr[i] = new T[columns];
+		}
+	}
+	template <typename T>
+	size_t Matrix<T>::InvalidValueArray(const std::vector<std::vector<T>>& arr)
+	{
+		size_t amountOfRows = arr.size();
+		size_t currentSizeArr = 0;
+		bool firstElement = true;
+		for (auto rowArr : arr) {
+			if (firstElement == true) {
+				currentSizeArr = rowArr.size();
+				firstElement = false;
+			}
+			else {
+				if (rowArr.size() != currentSizeArr) {
+					throw std::invalid_argument("Invalid index");
+					break;
+				}
+			}
+		}
+		return currentSizeArr;
+	}
 }
-
-
--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include "Matrix.hpp"
+#include "dVector.hpp"
 
 int main() {
-	std::vector<std::vector<double>> arr = { {1,0},{2,0} };
-	std::vector<std::vector<double>> arr1 = { {1,4},{2,7} };
-	Matrix<double> matr(arr);
-	Matrix<double> matr1(arr1); 
-	std::cout << matr[0][0] << std::endl;
-	matr.PrintMatrix(); 
-	matr[1][0] = 3; 
-	matr.PrintMatrix(); 
 	//            1 2 3
 	//  matrix =  4 5 6 if u wanna to get the last elements in the first 
-	//            7 8 9 row u have to matrix[0][2];  
+	//            7 8 9 row u have to matrix[0][2]; 
+	std::vector<double> arr = { 1, 1, 1 };
+	dVector::dVector3d vec(arr);
+	dVector::dVector3d vec_1(1, 1, 2); 
+	dVector::dVector3d vec_2 = vec + vec_1; 
+	vec_2.PrintVector(); 
 	return 0;
 }
